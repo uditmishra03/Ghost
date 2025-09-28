9b4295ab61742fe8c57a37d7f080943a829b9580
Mishra, Udit <udit.mishra2@wipro.com>
2025-09-29 00:56:07 +0530

    testing rollback and versioning

--- apps/admin-x-settings/src/App.tsx
+++ apps/admin-x-settings/src/App.tsx
@@ -1,11 +1,11 @@
 import MainContent from './MainContent';
 import NiceModal from '@ebay/nice-modal-react';
-import SettingsAppProvider, {OfficialTheme, UpgradeStatusType} from './components/providers/SettingsAppProvider';
-import SettingsRouter, {loadModals, modalPaths} from './components/providers/SettingsRouter';
-import {DesignSystemApp, DesignSystemAppProps} from '@tryghost/admin-x-design-system';
-import {FrameworkProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
-import {RoutingProvider} from '@tryghost/admin-x-framework/routing';
-import {ZapierTemplate} from './components/settings/advanced/integrations/ZapierModal';
+import SettingsAppProvider, { OfficialTheme, UpgradeStatusType } from './components/providers/SettingsAppProvider';
+import SettingsRouter, { loadModals, modalPaths } from './components/providers/SettingsRouter';
+import { DesignSystemApp, DesignSystemAppProps } from '@tryghost/admin-x-design-system';
+import { FrameworkProvider, TopLevelFrameworkProps } from '@tryghost/admin-x-framework';
+import { RoutingProvider } from '@tryghost/admin-x-framework/routing';
+import { ZapierTemplate } from './components/settings/advanced/integrations/ZapierModal';
 
 interface AppProps {
     framework: TopLevelFrameworkProps;
@@ -15,15 +15,16 @@ interface AppProps {
     upgradeStatus?: UpgradeStatusType;
 }
 
-function App({framework, designSystem, officialThemes, zapierTemplates, upgradeStatus}: AppProps) {
+function App({ framework, designSystem, officialThemes, zapierTemplates, upgradeStatus }: AppProps) {
     return (
         <FrameworkProvider {...framework}>
             <SettingsAppProvider officialThemes={officialThemes} upgradeStatus={upgradeStatus} zapierTemplates={zapierTemplates}>
                 {/* NOTE: we need to have an extra NiceModal.Provider here because the one inside DesignSystemApp
                     is loaded too late for possible modals in RoutingProvider, and it's quite hard to change it at
-                    this point */}
+                    this point 
+                    some comment to test the version .*/}
                 <NiceModal.Provider>
-                    <RoutingProvider basePath='settings' modals={{paths: modalPaths, load: loadModals}}>
+                    <RoutingProvider basePath='settings' modals={{ paths: modalPaths, load: loadModals }}>
                         <DesignSystemApp className='admin-x-settings' {...designSystem}>
                             <SettingsRouter />
                             <MainContent />
